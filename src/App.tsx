3155daf125303a07b98905f7fa012eaa972f3f60
jfaulk3 <faulknerjimmie@gmail.com>
2021-04-24 12:42:34 -0500

    init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+const hyrule_compendium = require("hyrule-compendium");
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  let comp = new hyrule_compendium.compendium();
+
+  comp.get_all(console.log); // get all entries
+  return <h1>Hello World</h1>;
 }
 
 export default App;
