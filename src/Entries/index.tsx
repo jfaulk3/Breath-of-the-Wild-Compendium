2c0552a258c5d4c07c15f6aeb9cb8ab5c3ea3b0a
jfaulk3 <faulknerjimmie@gmail.com>
2021-04-27 20:04:07 -0500

    Connected API to application

--- src/Entries/index.tsx
+++ src/Entries/index.tsx
@@ -2,19 +2,23 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 interface entry {
-  category: string;
-  common_locations: Array<string>;
-  description: string;
-  drops: Array<string>;
-  id: number;
-  image: string;
-  name: string;
+  attack?: number;
+  category?: string;
+  common_locations?: Array<string>;
+  defense?: number;
+  cooking_effect?: string;
+  description?: string;
+  hearts_recovered?: number;
+  drops?: Array<string>;
+  id?: number;
+  image?: string;
+  name?: string;
 }
 
 function Entries() {
   const BASE_URL = "https://botw-compendium.herokuapp.com/api/v2/category/";
   const { categoryName }: { categoryName: string } = useParams();
-  const [list, setList] = useState<entry[]>([] as entry[]);
+  const [list, setList] = useState([] as entry[]);
 
   useEffect(() => {
     if (categoryName) {
@@ -23,8 +27,9 @@ function Entries() {
         try {
           const url = BASE_URL + categoryName;
           const response = await fetch(url, { signal: abortController.signal });
-          const data: object = await response.json();
-          setList(Object.values(data));
+          const results: { data: Array<any> } = await response.json();
+          console.log("results: ", results.data);
+          setList(results.data);
           console.log("API Request");
         } catch (error) {
           if (error.name === "AbortError") {
@@ -41,18 +46,48 @@ function Entries() {
   }, [categoryName]);
 
   if (list) {
+    console.log(list);
     return (
       <div>
         <div>
           <h2>{categoryName.toUpperCase()}</h2>
         </div>
-        <ul>
-          {list.map((element, index) => (
-            <li key={index}>
-              {element.name}: {index}
-            </li>
-          ))}
-        </ul>
+        <div>
+          {list.map(
+            ({
+              attack = null,
+              common_locations = [],
+              defense = null,
+              cooking_effect = "",
+              description = "",
+              hearts_recovered = null,
+              drops = [],
+              id = null,
+              image = "",
+              name = "",
+            }) => (
+              <div key={id}>
+                {id}: {name} <br />{" "}
+                {cooking_effect ? `Effect: ${cooking_effect}` : ""} <br />{" "}
+                {attack !== null ? `Attack: ${attack}` : ""} <br />{" "}
+                {defense !== null ? `Defense: ${defense}` : ""} <br />{" "}
+                {description} <br />{" "}
+                {hearts_recovered !== null
+                  ? `Hearts recovered: ${hearts_recovered}`
+                  : ""}{" "}
+                <br />
+                {common_locations.map((location) => (
+                  <div key={location}> {location} | </div>
+                ))}
+                <br />{" "}
+                {drops.map((drop) => (
+                  <div key={drop}> {drop} | </div>
+                ))}{" "}
+                <br /> <img src={image} alt={name} /> <br /> <br />
+              </div>
+            )
+          )}
+        </div>
       </div>
     );
   } else {
